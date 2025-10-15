64709576aef547363e3689227d864b0cb9504fb5
sharmashiv24251 <sharmashiv24251@gmail.com>
2025-10-15 23:50:56 +0530

    rainbow kit wallet connection

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
 import { WalletIcon } from "./svg-icons";
 import Button from "./UI/Button";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Nav = () => {
   return (
@@ -19,8 +20,108 @@ const Nav = () => {
       <div className="h-full p-3 sm:p-3 pl-1.5">
         <Button size="sm" round variant="primary">
           <div className="flex flex-row items-center gap-1.5">
-            <WalletIcon />
-            <span>Connect Wallet</span>
+            <ConnectButton.Custom>
+              {({
+                account,
+                chain,
+                openAccountModal,
+                openChainModal,
+                openConnectModal,
+                authenticationStatus,
+                mounted,
+              }) => {
+                // Note: If your app doesn't use authentication, you
+                // can remove all 'authenticationStatus' checks
+                const ready = mounted && authenticationStatus !== "loading";
+                const connected = ready && account && chain;
+
+                return (
+                  <div
+                    {...(!ready && {
+                      "aria-hidden": true,
+                      style: {
+                        opacity: 0,
+                        pointerEvents: "none",
+                        userSelect: "none",
+                      },
+                    })}
+                  >
+                    {(() => {
+                      if (!connected) {
+                        return (
+                          <button
+                            onClick={openConnectModal}
+                            type="button"
+                            className="flex flex-row items-center gap-1.5"
+                          >
+                            <WalletIcon />
+                            <span>Connect Wallet</span>
+                          </button>
+                        );
+                      }
+
+                      if (chain.unsupported) {
+                        return (
+                          <button
+                            onClick={openChainModal}
+                            type="button"
+                            className="flex flex-row items-center gap-1.5"
+                          >
+                            <WalletIcon />
+                            <span>Wrong network</span>
+                          </button>
+                        );
+                      }
+
+                      return (
+                        <div style={{ display: "flex", gap: 12 }}>
+                          {/* <button
+                            onClick={openChainModal}
+                            style={{ display: "flex", alignItems: "center" }}
+                            type="button"
+                          >
+                            {chain.hasIcon && (
+                              <div
+                                style={{
+                                  background: chain.iconBackground,
+                                  width: 12,
+                                  height: 12,
+                                  borderRadius: 999,
+                                  overflow: "hidden",
+                                  marginRight: 4,
+                                }}
+                              >
+                                {chain.iconUrl && (
+                                  <img
+                                    alt={chain.name ?? "Chain icon"}
+                                    src={chain.iconUrl}
+                                    style={{ width: 12, height: 12 }}
+                                  />
+                                )}
+                              </div>
+                            )}
+
+                            {chain.name}
+                          </button> */}
+
+                          <button
+                            onClick={openAccountModal}
+                            type="button"
+                            className="flex flex-row items-center gap-1.5 cursor-pointer"
+                          >
+                            <WalletIcon />
+                            {account.displayName}
+                            {/* {account.displayBalance
+                              ? ` (${account.displayBalance})`
+                              : ""} */}
+                          </button>
+                        </div>
+                      );
+                    })()}
+                  </div>
+                );
+              }}
+            </ConnectButton.Custom>
           </div>
         </Button>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,16 @@ import "./index.css";
 import App from "./App.tsx";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base } from "wagmi/chains";
+
+const config = getDefaultConfig({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,8 +29,12 @@ const queryClient = new QueryClient({
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>
 );
